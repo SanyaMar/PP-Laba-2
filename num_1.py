89816f638202ee018e2eb83e5920441428556097
SanyaMar <eklisof@gmail.com>
2023-12-03 17:23:30 +0300

    Вторая подобная функция

--- num_1.py
+++ num_1.py
@@ -8,4 +8,14 @@ def getting_absolute_path(images_class: str) :
     image_names = os.listdir(class_path)
     name=map(lambda name: os.path.join(class_path, name), image_names)
     image_absolute_path = list(name)
-    return image_absolute_path
+    return image_absolute_path#список с путями на каждый элемент
+
+
+def getting_relative_path(images_class:str):
+    absolute_path = os.path.relpath('dataset')
+    class_path = os.path.join(absolute_path, images_class)
+    image_names = os.listdir(class_path)
+    name=map(lambda name: os.path.join(class_path, name), image_names)
+    image_absolute_path = list(name)
+    return image_absolute_path#список с путями на каждый элемент
+
