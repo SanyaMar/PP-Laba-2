bd60cef6df473c0a12f5585db59c66613bf8a724
SanyaMar <eklisof@gmail.com>
2023-12-03 17:01:11 +0300

    Начало работы. Первая функция

---
+++ num_1.py
@@ -0,0 +1,11 @@
+import os
+from typing import List
+
+# Функция должна возвращять список абсолютных путей для изображений
+def getting_absolute_path(images_class: str) : 
+    absolute_path = os.path.abspath('dataset')
+    class_path = os.path.join(absolute_path, images_class)
+    image_names = os.listdir(class_path)
+    name=map(lambda name: os.path.join(class_path, name), image_names)
+    image_absolute_path = list(name)
+    return image_absolute_path
