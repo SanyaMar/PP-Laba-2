6e4a7418c3572e9f6274d1764b74c45cf46a5046
SanyaMar <eklisof@gmail.com>
2023-12-04 16:59:39 +0300

    Работает как надо

--- num_1.py
+++ num_1.py
@@ -11,6 +11,7 @@ def getting_absolute_path(images_class: str) -> list[str]:
     image_names = os.listdir(class_path)
     name=map(lambda name: os.path.join(class_path, name), image_names)
     image_absolute_path = list(name)
+
     return image_absolute_path#список с путями на каждый элемент
 
 
@@ -18,27 +19,28 @@ def getting_relative_path(images_class:str) -> list[str]:
     '''
     Данная функция получает имя класса изображений и возвращает список относительных путей изображений(относительно dataset)
     '''
-    absolute_path = os.path.relpath('dataset_1')
-    class_path = os.path.join(absolute_path, images_class)
+    relative_path = os.path.relpath('dataset_1')
+    class_path = os.path.join(relative_path, images_class)
     image_names = os.listdir(class_path)
     name=map(lambda name: os.path.join(class_path, name), image_names)
-    image_absolute_path = list(name)
-    return image_absolute_path#список с путями на каждый элемент
+    image_relative_path = list(name)
+
+    return image_relative_path#список с путями на каждый элемент
 
 def main()-> None:
-    first_class="dog"
-    second_class="cat"
+    first_class="cat"
+    second_class="dog"
 
-    dog_abs_paths = getting_absolute_path(first_class)
-    dog_rel_paths = getting_relative_path(first_class)
-    cat_abs_paths = getting_absolute_path(second_class)
-    cat_rel_paths = getting_relative_path(second_class)
+    cat_abs_paths = getting_absolute_path(first_class)
+    cat_rel_paths = getting_relative_path(first_class)
+    dog_abs_paths = getting_absolute_path(second_class)
+    dog_rel_paths = getting_relative_path(second_class)
 
     with open('paths.csv', 'w') as csv_file:
         writer = csv.writer(csv_file, delimiter=',', lineterminator='\r')
-        for abs_path, rel_path in zip(dog_abs_paths, dog_rel_paths):
-            writer.writerow([abs_path, rel_path, first_class])
         for abs_path, rel_path in zip(cat_abs_paths, cat_rel_paths):
+            writer.writerow([abs_path, rel_path, first_class])
+        for abs_path, rel_path in zip(dog_abs_paths, dog_rel_paths):
             writer.writerow([abs_path, rel_path, second_class])
 
 
--- num_2.py
+++ num_2.py
@@ -1,8 +1,28 @@
 import os
+import csv
+import shutil
 
+def replace_images(class_name: str) -> list[str]:
+    
+    relative_path = os.path.relpath('dataset_2')
+    class_path = os.path.join(relative_path, class_name)
+    image_names = os.listdir(class_path)
+    names=map(lambda name: os.path.join(class_path, name), image_names)
+    image_rel_paths = list(names)
+    new=map(lambda name: os.path.join(relative_path, f'{class_name}_{name}'), image_names)
+    new_image_rel_paths = list(new)
+    for old, new in zip(image_rel_paths, new_image_rel_paths):
+        os.replace(old, new)
+
+    os.chdir('dataset_2')
+
+    if os.path.isdir(class_name):
+        os.rmdir(class_name)
+
+    os.chdir('..')
 
 def getting_absolute_path(class_name: str) -> list[str]:
-    
+
     absolute_path = os.path.abspath('dataset_2')
     image_names = os.listdir(absolute_path)
     image_class_names = []
@@ -15,7 +35,7 @@ def getting_absolute_path(class_name: str) -> list[str]:
     return image_absolute_path
 
 def getting_relative_path(class_name: str)-> list[str]:
-    
+   
     absolute_path = os.path.relpath('dataset_2')
     image_names = os.listdir(absolute_path)
     image_class_names = []
@@ -26,3 +46,35 @@ def getting_relative_path(class_name: str)-> list[str]:
     image_absolute_path = list(name)
     
     return image_absolute_path
+
+
+def main()-> None:
+    first_class="cat"
+    second_class="dog"
+
+    if os.path.isdir('dataset_2'):
+        shutil.rmtree('dataset_2')
+
+    path_dataset_1 = os.path.relpath('dataset_1')
+    path_dataset_2 = os.path.relpath('dataset_2')
+    shutil.copytree(path_dataset_1, path_dataset_2)
+
+    replace_images(first_class)
+    replace_images(second_class)
+
+    dog_abs_paths = getting_absolute_path(first_class)
+    dog_rel_paths = getting_relative_path(first_class)
+    cat_abs_paths = getting_absolute_path(second_class)
+    cat_rel_paths = getting_relative_path(second_class)
+
+    with open('paths_2.csv', 'w') as csv_file:
+        writer = csv.writer(csv_file, delimiter=',', lineterminator='\r')
+        for abs_path, rel_path in zip(dog_abs_paths, dog_rel_paths):
+            writer.writerow([abs_path, rel_path, first_class])
+        for abs_path, rel_path in zip(cat_abs_paths, cat_rel_paths):
+            writer.writerow([abs_path, rel_path, second_class])
+
+
+
+if __name__ == "__main__":
+    main()
