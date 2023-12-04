bf7760dbfb64d1962fc0255f9c27764b551dcd44
SanyaMar <eklisof@gmail.com>
2023-12-04 14:40:43 +0300

    первый номер работает

--- num_1.py
+++ num_1.py
@@ -1,4 +1,6 @@
 import os
+import csv
+
 from typing import List
 
 # Функция должна возвращять список абсолютных путей для изображений
@@ -19,3 +21,22 @@ def getting_relative_path(images_class:str):
     image_absolute_path = list(name)
     return image_absolute_path#список с путями на каждый элемент
 
+def main():
+    first_class="dog"
+    second_class="cat"
+
+    dog_abs_paths = getting_absolute_path(first_class)
+    dog_rel_paths = getting_relative_path(first_class)
+    cat_abs_paths = getting_absolute_path(second_class)
+    cat_rel_paths = getting_relative_path(second_class)
+
+    with open('paths.csv', 'w') as csv_file:
+        writer = csv.writer(csv_file, delimiter=',', lineterminator='\r')
+        for full_path, rel_path in zip(dog_abs_paths, dog_rel_paths):
+            writer.writerow([full_path, rel_path, first_class])
+        for full_path, rel_path in zip(cat_abs_paths, cat_rel_paths):
+            writer.writerow([full_path, rel_path, second_class])
+
+
+if __name__ == "__main__":
+    main()
